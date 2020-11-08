5c07f792b4cd677e664833d70b3ab09b2ccefe98
proff001 <thommhole@gmail.com>
2020-11-08 03:00:00 +0100

    Ferdig med lyskrys med 4 lys, men uten fotgjengerovergang.

--- Trafikklys/trafikklys.ino
+++ Trafikklys/trafikklys.ino
@@ -1,52 +1,68 @@
-const int red[4] = {A0, A3, 6, 11};
-const int green[4] = {A1, A4, 5, 10};
-const int yellow[4]= {A2, A5, 3, 9};
+int horizState = 0;
+int vertState = 0;
+int lightMode = 0;
+
+const int lightState[4][4] = {
+    {0, 1, 1},
+    {0, 0, 1},
+    {1, 1, 0},
+    {1, 0, 1}
+};
+
+const int lights[4][3] = {
+    {2, 3, 4},
+    {5, 6, 7},
+    {8, 9, 10},
+    {11, 12, 13},
+};
+
+const float time[4] = {
+    0.5, // Tid fra rødt til rødt gult
+    2, // Tid fra rødt gult til grønnt
+    6, // Tid fra grønnt til gult
+    2.5  // Tid fra gult til rødt
+};
 
 void setup() {
-    for(int i = 0; i <= 3; i++) {
-        pinMode(red[i], OUTPUT);
-    }
-    for(int i = 0; i <= 3; i++) {
-        pinMode(green[i], OUTPUT);
-    }
-    for(int i = 0; i <= 3; i++) {
-        pinMode(yellow[i], OUTPUT);
-    }
-}
-
-void setColor(int cross, char color) {
-    if(cross % 2 != 0) {
-        if(color == 'r') {
-            for(int i = 0; i <= 3; i++) {
-                if(i % 2 != 0) {
-                    digitalWrite(red[i], true);
-                    digitalWrite(green[i], false);
-                    digitalWrite(yellow[i], false);
-                } else {
-                    digitalWrite(red[i], false);
-                    digitalWrite(red[i], true);
-                    digitalWrite(red[i], true);
-                }
-            }
-        } else if(color == 'g') {
-            for(int i = 0; i <= 3; i++) {
-                if(cross % 2 != 0) {
-                    
-                }
-                if(i % 2 != 0) {
-                    digitalWrite(red[i], true);
-                    digitalWrite(green[i], false);
-                    digitalWrite(yellow[i], false);
-                } else {
-                    digitalWrite(red[i], false);
-                    digitalWrite(red[i], true);
-                    digitalWrite(red[i], true);
-                }
-            }
+    for(int i = 0; i < 5; i++) {
+        for(int j = 0; j < 4; j++) {
+            pinMode(lights[i][j], OUTPUT);
         }
     }
 }
 
+void Trafikklys() {
+    for(int i = 0; i < int((sizeof(lightState))/(sizeof(*lightState))); i++) {
+        vertState = i;
+        horizState = 0;
+        UpdateLEDs(time[i] * 1000);
+    }
+
+    for(int i = 0; i < int((sizeof(lightState))/(sizeof(*lightState))); i++) {
+        vertState = 0;
+        horizState = i;
+        UpdateLEDs(time[i] * 1000);
+    }
+}
+
+void UpdateLEDs(int time) {
+    for(int i = 0; i < int((sizeof(lights))/(sizeof(lights[0]))); i++) {
+        lightMode = 0;
+
+        if (i <= 1) {
+            lightMode = vertState;
+        } else {
+            lightMode = horizState;
+        }
+
+        for(int j = 0; j < 3; j++) {
+            digitalWrite(lights[i][j], lightState[lightMode][j]);
+        }
+    }
+
+    delay(time);
+}
+
 void loop() {
-    
+    Trafikklys();
 }
