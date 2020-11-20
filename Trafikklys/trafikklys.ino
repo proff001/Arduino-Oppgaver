177d6603ff10b7343dc55f389c5fc7f6d07b3f23
proff001 <thommhole@gmail.com>
2020-11-20 12:02:35 +0100

    Fotgjengerovergang er nå funksjoneltgti add .!

--- Trafikklys/trafikklys.ino
+++ Trafikklys/trafikklys.ino
@@ -1,12 +1,23 @@
 int horizState = 0;
 int vertState = 0;
 int lightMode = 0;
+int walkMode = 0;
 
 const int lightState[4][4] = {
     {0, 1, 1}, // Rødt
     {0, 0, 1}, // Rødt gult
     {1, 1, 0}, // Grønnt
     {1, 0, 1}  // Gult
+};
+
+const int walkLightState[2][2] = {
+    {0, 1}, // Fotgjengerovergang Rødt
+    {1, 0} // Fotgjengerovergang Grønnt
+};
+
+const int walkLights[2][2] = {
+    {A0, A1}, // Fotgjengerovergang Øst
+    {A2, A3} // Fotgjengerovergang Nord
 };
 
 const int lights[4][3] = {
@@ -29,6 +40,11 @@ void setup() {
             pinMode(lights[i][j], OUTPUT);
         }
     }
+    for(int i = 0; i < 2; i++) {
+        for(int j = 0; j < 2; j++) {
+            pinMode(walkLights[i][j], OUTPUT);
+        }
+    }
 }
 
 void Trafikklys() {
@@ -37,7 +53,6 @@ void Trafikklys() {
         horizState = 0;
         UpdateLEDs(time[i] * 1000);
     }
-
     for(int i = 0; i < int((sizeof(lightState))/(sizeof(*lightState))); i++) {
         vertState = 0;
         horizState = i;
@@ -48,13 +63,29 @@ void Trafikklys() {
 void UpdateLEDs(int time) {
     for(int i = 0; i < int((sizeof(lights))/(sizeof(lights[0]))); i++) {
         lightMode = 0;
-
         if (i <= 1) {
             lightMode = vertState;
+            if (vertState != 2) {
+                for(int j = 0; j < 2; j++) {
+                    digitalWrite(walkLights[0][j], walkLightState[0][j]);
+                }
+            } else {
+                for(int j = 0; j < 2; j++) {
+                    digitalWrite(walkLights[0][j], walkLightState[1][j]);
+                }
+            }
         } else {
             lightMode = horizState;
+            if (horizState != 2) {
+                for(int j = 0; j < 2; j++) {
+                    digitalWrite(walkLights[1][j], walkLightState[0][j]);
+                }
+            } else {
+                for(int j = 0; j < 2; j++) {
+                    digitalWrite(walkLights[1][j], walkLightState[1][j]);
+                }
+            }
         }
-
         for(int j = 0; j < 3; j++) {
             digitalWrite(lights[i][j], lightState[lightMode][j]);
         }
