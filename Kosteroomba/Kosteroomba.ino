469249fdb79f4a84d457ef1a3cd1633776499ca0
proff001 <thommhole@gmail.com>
2021-03-22 12:28:03 +0100

    Testet vider med protothreads

--- Kosteroomba/Kosteroomba.ino
+++ Kosteroomba/Kosteroomba.ino
@@ -1,41 +1,58 @@
-const int trig = 2;
-const int echo = 3;
+#include <protothreads.h>
+
+#define trig 2
+#define echo 3
+#define led 4
+
+pt pt1, pt2; 
 
 int dist = 0;
 
-int time[3] = {0, 0, 0};
+int GetDist(struct pt *pt) {
+    PT_BEGIN(pt);
 
-int currentTime = 0;
+    dist = 0;
+    digitalWrite(trig, 0);
+    
+    PT_SLEEP(pt, 2);
+    digitalWrite(trig, 1);
+    PT_SLEEP(pt, 15);
+    digitalWrite(trig, 0);
+    const unsigned long duration = pulseIn(echo, 1);
+    dist = duration/29/2;
+    if(duration == 0) {
+        Serial.println("Warning: No pulse recived from sensor!");
+    } else {
+        Serial.print("Distance to nearest Object: ");
+        Serial.print(dist);
+        Serial.println(" cm");
+    }
+
+    PT_END(pt);
+}
+
+int BlinkLed(pt *pt) {
+    PT_BEGIN(pt);
+    
+        blinkTime = millis();
+        PT_WAIT_UNTIL(pt, millis() - blinkTime > 1000);
+        digitalWrite(led, true);
+        blinkTime = millis();
+        PT_WAIT_UNTIL(pt, millis() - blinkTime > 1000);
+        digitalWrite(led, false);    
+
+    PT_END(pt);
+}
 
 void setup() {
     pinMode(trig, OUTPUT);
     pinMode(echo, INPUT);
-
     Serial.begin(9600);
+    PT_INIT(&pt1);
+    PT_INIT(&pt2);
 }
 
 void loop() {
-    currentTime = millis();
-    if(time[0] == 0 || time[0] <= currentTime) {
-        dist = 0;
-        digitalWrite(trig, 0);
-        currentTime = millis();
-        if(time[1] == 0 || time[1] <= currentTime) {
-            
-        }
-        delayMicroseconds(15);
-        digitalWrite(trig, 0);
-
-        const unsigned long duration = pulseIn(echo, 1);
-        dist = duration/29/2;
-        if(duration == 0) {
-            Serial.println("Warning: No pulse recived from sensor!");
-        } else {
-            Serial.print("Distance to nearest Object: ");
-            Serial.print(dist);
-            Serial.println(" cm");
-        }
-        digitalWrite(trig, 1);
-    }
-    time[0] == currentTime + 100;
+    GetDist(&pt1);
+    BlinkLed(&pt2);
 }
