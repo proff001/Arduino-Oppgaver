a73cfbdcda12e15c571018397a31690301ff5f9e
proff001 <thommhole@gmail.com>
2022-03-17 12:42:22 +0100

    Ferdig med LCD versjonen av vannsensor oppgaven

--- Vannsensor/VannsensorLCD.ino
+++ Vannsensor/VannsensorLCD.ino
@@ -6,9 +6,10 @@
 #define WATER100 A3
 #define MOTOR 8
 
+unsigned long lastChange = 0;
 LiquidCrystal lcd(6, 7, 2, 3, 4, 5);
 String currentText;
-bool pump = false;
+bool lastState[4] = {false, false, false, false};
 
 void setup() {
 	lcd.begin(16, 2);
@@ -25,26 +26,38 @@ void loop() {
 	bool water50 = digitalRead(WATER50);
 	bool water75 = digitalRead(WATER75);
 	bool water100 = digitalRead(WATER100);
+	bool pump = digitalRead(MOTOR);
 
 	if((!water25 && (water50 || water75 || water100)) || (!water50 && (water75 || water100)) || (!water75 && water100)) {
+		if(pump) digitalWrite(MOTOR, LOW);
 		display("Sensor feil");
-
-		if(pump) {
-			digitalWrite(MOTOR, LOW);
-			pump = false;
-		}
 	} else {
 		if(pump && water100) {
 			digitalWrite(MOTOR, LOW);
 			display("Pump stopped");
-			pump = false;
-			delay(1000);
-		} else if(!pump && water25 && !water50) {
+			delay(2000);
+		} else if(!pump && !water25) {
 			digitalWrite(MOTOR, HIGH);
 			display("Pump started");
-			pump = true;
-			delay(1000);
+			delay(2000);
 		}
+
+		if(!pump && (lastState[0] != water25 || lastState[1] != water50 || lastState[2] != water75 || lastState[3] != water100)) {
+			if(lastChange > millis()) {
+				digitalWrite(MOTOR, HIGH);
+				display("Unusual usage");
+				delay(1000);
+				display("Pump started");
+				delay(1000);
+			}
+
+			lastChange = millis() + 5000;
+		}
+
+		lastState[0] = water25;
+		lastState[1] = water50;
+		lastState[2] = water75;
+		lastState[3] = water100;
 
 		display(water100 ? "100%" : water75 ? "75%" : water50 ? "50%" : water25 ? "25%" : "0%");
 	}
@@ -55,7 +68,7 @@ void loop() {
 void display(String text) {
 	if(text == currentText) return;
 	currentText = text;
-	
+
 	lcd.clear();
 	lcd.setCursor(0, 0);
 	lcd.print("Monitoring:");
